cdf2bac1fb52bc5be5d52df709ee687defb1a6b3
101danny <160006806+101danny@users.noreply.github.com>
2024-09-23 09:15:48 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 💖 | Endy Bot ]";
+const doNotDelete = "[ 🌐 | Endy Bot ]";
 /**
 * @author NTKhang
 * @author: do not delete it
