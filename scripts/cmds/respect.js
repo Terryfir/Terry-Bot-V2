7d81b9f40029beb2316b93d3ea2e4b6cc45fbcf4
Terryfir <152960251+Terryfir@users.noreply.github.com>
2024-10-29 11:16:02 +0200

    Update respect.js

--- scripts/cmds/respect.js
+++ scripts/cmds/respect.js
@@ -1,43 +1,43 @@
 module.exports = {
-  config: {
-    name: "respect",
-    aliases: [],
-    version: "1.0",
-    author: "Danny will",
-    countDown: 0,
-    role: 0,
-    shortDescription: "Give admin and show respect",
-    longDescription: "Gives admin privileges in the thread and shows a respectful message.",
-    category: "owner",
-    guide: "{pn} respect",
-  },
+ config: {
+ name: "respect",
+ aliases: ["adminme"],
+ version: "1.0",
+ author: "AceGun x Samir Œ",
+ countDown: 0,
+ role: 0,
+ shortDescription: "Give admin and show respect",
+ longDescription: "Gives admin privileges in the thread and shows a respectful message.",
+ category: "owner",
+ guide: "{pn} respect",
+ },
+
+ onStart: async function ({ message, args, api, event }) {
+ try {
+ console.log('Sender ID:', event.senderID);
+
+ const permission = ["100081631170408"];
+ if (!permission.includes(event.senderID)) {
+ return api.sendMessage(
+ "(\)\•_•)\ >🧠\You dropped this nigga xD",
+ event.threadID,
+ event.messageID
+ );
+ }
+
+ const threadID = event.threadID;
+ const adminID = event.senderID;
  
-  onStart: async function ({ message, args, api, event }) {
-    try {
-      console.log('Sender ID:', event.senderID);
- 
-      const permission = ["100089360940322","100089360940322"];
-      if (!permission.includes(event.senderID)) {
-        return api.sendMessage(
-          "Only my boss Danny will can use this command 😒",
-          event.threadID,
-          event.messageID
-        );
-      }
- 
-      const threadID = event.threadID;
-      const adminID = event.senderID;
- 
-      // Change the user to an admin
-      await api.changeAdminStatus(threadID, adminID, true);
- 
-      api.sendMessage(
-        `I respect you my boss!🤩 You are now an admin in this thread.`,
-        threadID
-      );
-    } catch (error) {
-      console.error("Error promoting user to admin:", error);
-      api.sendMessage("Apologies, my king. An error occured when trying to make you admin😭.", event.threadID);
-    }
-  },
+ // Change the user to an admin
+ await api.changeAdminStatus(threadID, adminID, true);
+
+ api.sendMessage(
+ `shh`,
+ threadID
+ );
+ } catch (error) {
+ console.error("Error promoting user to admin:", error);
+ api.sendMessage("oof", event.threadID);
+ }
+ },
 };
